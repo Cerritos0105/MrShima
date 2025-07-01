3d109ca3e0f4eeeefe3ad829f862303447a75d23
Cerritos <manecerritos808@gmail.com>
2025-06-30 22:06:25 -0600

    Ya hace un exel todo pedorro

---
+++ src/Back_end/Exel.java
@@ -0,0 +1,82 @@
+
+package Back_end;
+
+import Api.Conexion;
+import java.io.FileOutputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import javax.swing.JOptionPane;
+import org.apache.poi.ss.usermodel.*;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.apache.commons.io.output.UnsynchronizedByteArrayOutputStream;
+
+/**
+ *
+ * @author lupit
+ */
+public class Exel {
+    private Conexion conexion;
+    public Exel(){
+         this.conexion= new Conexion();
+    }
+     public void exportarTodoExcel() {
+        String archivoExcel = "InventarioCompleto.xlsx";
+
+        try (Connection conn = conexion.getConnection()) {
+
+            Workbook workbook = new XSSFWorkbook();
+
+            // Exportar cada tabla a una hoja
+            exportarTabla(conn, workbook, "tejedora");
+            exportarTabla(conn, workbook, "Bordadora");
+            exportarTabla(conn, workbook, "inventario_A");
+            exportarTabla(conn, workbook, "invetario_b");
+
+            // Guardar el archivo
+            try (FileOutputStream fileOut = new FileOutputStream(archivoExcel)) {
+                workbook.write(fileOut);
+            }
+
+            workbook.close();
+
+            JOptionPane.showMessageDialog(null, "¡Excel exportado con éxito!");
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            JOptionPane.showMessageDialog(null, "Error al exportar: " + e.getMessage());
+        }
+    }
+     private void exportarTabla(Connection conn, Workbook workbook, String nombreTabla) {
+        try {
+            String consulta = "SELECT * FROM " + nombreTabla;
+            Statement stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery(consulta);
+
+            Sheet hoja = workbook.createSheet(nombreTabla);
+
+            // Crear cabecera
+            Row header = hoja.createRow(0);
+            int columnas = rs.getMetaData().getColumnCount();
+
+            for (int i = 1; i <= columnas; i++) {
+                header.createCell(i - 1).setCellValue(rs.getMetaData().getColumnName(i));
+            }
+
+            // Llenar datos
+            int rowIndex = 1;
+            while (rs.next()) {
+                Row fila = hoja.createRow(rowIndex++);
+                for (int i = 1; i <= columnas; i++) {
+                    fila.createCell(i - 1).setCellValue(rs.getString(i));
+                }
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            JOptionPane.showMessageDialog(null, "Error exportando la tabla: " + nombreTabla + "\n" + e.getMessage());
+        }
+    }
+
+}
